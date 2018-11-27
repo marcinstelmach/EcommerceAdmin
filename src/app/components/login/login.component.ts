652a0d3d6755146b9ae203f37d83a5251a1457ac
Marcin Stelmach <marcinxd4@gmail.com>
2018-11-27 20:05:52 +0100

    Shipment managing

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {HttpErrorResponse} from '@angular/common/http';
 import {Router} from '@angular/router';
 import {AuthService} from 'app/services/auth/auth.service';
 import {UserService} from '../../services/user/user.service';
