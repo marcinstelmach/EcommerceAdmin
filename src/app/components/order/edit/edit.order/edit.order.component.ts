212e6a6c0bce67725becbc4037a7ea8146715526
Marcin Stelmach <marcinxd4@gmail.com>
2019-02-28 22:38:49 +0100

    Updating order status

--- src/app/components/order/edit/edit.order/edit.order.component.ts
+++ src/app/components/order/edit/edit.order/edit.order.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {Order} from '../../../../models/order-interface';
 import {OrderService} from '../../../../services/order/order.service';
-import {MatTableDataSource} from '@angular/material';
+import {MatSnackBar, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-edit.order',
@@ -16,7 +16,9 @@ export class EditOrderComponent implements OnInit {
   orderTableColumns: string[] = ['position', 'creationDateTime', 'finalPrice', 'basePrice', 'shipmentPrice', 'isPayed', 'isShipped',
     'isClosed', 'payedDateTime', 'closedDateTime'];
 
-  constructor(private router: Router, private orderService: OrderService) { }
+  constructor(private router: Router,
+              private orderService: OrderService,
+              private updatedAlert: MatSnackBar) { }
 
   ngOnInit() {
     this.orderId = this.router.url.split('/')[2];
@@ -33,5 +35,24 @@ export class EditOrderComponent implements OnInit {
 
   buildOrderTable(order: Order) {
     this.orderTable = new MatTableDataSource<Order>([order]);
+  }
+
+  update(property: string) {
+    if (property === 'payed') {
+      this.order.isPayed = !this.order.isPayed;
+    }
+    if (property === 'closed') {
+      this.order.isClosed = !this.order.isClosed;
+    }
+    if (property === 'shipped') {
+      this.order.isShipped = !this.order.isShipped;
+    }
+
+    this.orderService.update(this.order).subscribe(data => {
+      this.updatedAlert.open('Updated !', 'Close', {
+        duration: 2000
+      });
+      this.getOrder();
+    });
   }
 }
--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {API_ORDERS} from '../../constants/enpoints';
+import {Order} from '../../models/order-interface';
 
 @Injectable()
 export class OrderService {
@@ -11,18 +12,40 @@ export class OrderService {
   getFiltered(take = 10, id?, dateFrom?, dateTo?, isShipped?, isPayed?, isClosed?): Observable<any> {
     let url = `${API_ORDERS}?take=${take}`;
 
-    if (id !== undefined) {url += `&id=${id}`; }
-    if (dateFrom !== undefined) {url += `&dateFrom=${dateFrom}`; }
-    if (dateTo !== undefined) {url += `&dateTo=${dateTo}`; }
-    if (isShipped !== undefined) {url += `&isShipped=${isShipped}`; }
-    if (isPayed !== undefined) {url += `&isPayed=${isPayed}`; }
-    if (isClosed !== undefined) {url += `&isClosed=${isClosed}`; }
+    if (id !== undefined) {
+      url += `&id=${id}`;
+    }
+    if (dateFrom !== undefined) {
+      url += `&dateFrom=${dateFrom}`;
+    }
+    if (dateTo !== undefined) {
+      url += `&dateTo=${dateTo}`;
+    }
+    if (isShipped !== undefined) {
+      url += `&isShipped=${isShipped}`;
+    }
+    if (isPayed !== undefined) {
+      url += `&isPayed=${isPayed}`;
+    }
+    if (isClosed !== undefined) {
+      url += `&isClosed=${isClosed}`;
+    }
 
     return this.http.get(url);
   }
 
   get(id: string): Observable<any> {
     return this.http.get(`${API_ORDERS}/${id}`);
+  }
+
+  update(order: Order): Observable<any> {
+    const data = {
+      payed: order.isPayed,
+      shipped: order.isShipped,
+      closed: order.isClosed
+    };
+
+    return this.http.put(`${API_ORDERS}/${order.id}`, data);
   }
 
   // private setUrlNull(url: string): string {
